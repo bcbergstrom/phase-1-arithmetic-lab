f87fd46a67c7cb9b07c46f4aca308ebbed55a5aa
Benton Bergstrom <bbergstrom0524@gmail.com>
2024-06-01 18:25:44 -0600

    final commit

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+const num1 = 31
+const num2 = 2
+const num3 = 9
+const num4 = 5
+let multiply = num1*num2
+let random = Math.floor((Math.random()+1)*10)
+let mod = num3%num4
+let max = 20
+// Write your ode here
